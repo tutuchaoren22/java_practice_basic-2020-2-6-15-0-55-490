24d1375405873360746bab888bc6d4d0b33da3f2
duyuqian <15738381799@163.com>
2020-02-10 11:38:51 +0800

    [duyuqian] feat:create the Animall class with age&weight and methods

---
+++ 005_extends/src/main/java/Animal.java
@@ -0,0 +1,36 @@
+public class Animal {
+    int age;
+    double weight;
+
+    public Animal() {
+    }
+
+    public Animal(int age, double weight) {
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void eat() {
+
+    }
+
+    public void sleep() {
+
+    }
+}
