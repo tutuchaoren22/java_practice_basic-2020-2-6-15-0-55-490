af0cbf7e3dc04f892075f2cd6dab008f570cc19d
duyuqian <15738381799@163.com>
2020-02-10 11:42:53 +0800

    [duyuqian] feat:create the Bird class with flyspeed and fly methods

---
+++ 005_extends/src/main/java/Birds.java
@@ -0,0 +1,23 @@
+public class Birds extends Animal{
+    double flySpeed ;
+
+    public Birds() {
+    }
+
+    public Birds(int age, double weight, double flySpeed) {
+        super(age, weight);
+        this.flySpeed = flySpeed;
+    }
+
+    public double getFlySpeed() {
+        return flySpeed;
+    }
+
+    public void setFlySpeed(double flySpeed) {
+        this.flySpeed = flySpeed;
+    }
+
+    public void fly(){
+
+    }
+}
